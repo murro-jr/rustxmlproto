786c783cea0107c1cadbad854895c90a5f416ce2
John Rexes Murro <jrmurro@gmail.com>
2021-12-04 16:14:24 +0800

    Add formatter for Rust enum object

--- src/objectwriter.rs
+++ src/objectwriter.rs
@@ -4,6 +4,33 @@ use std::io::Write;
 
 use crate::prototype::{Member, ObjectType, Prototype};
 
+pub(crate) struct EnumFormatter;
+
+impl EnumFormatter {
+    pub fn format(members: Vec<Member>) -> String {
+        let mut result = "".to_string();
+
+        for member in members.iter() {
+            if let Some(datatype) = &member.datatype {
+                let member = match &datatype[..] {
+                    "generic" => format!("\t{},\n", member.name.to_case(Case::UpperSnake)),
+                    _ => format!(
+                        "\t{}({}),\n",
+                        member.name.to_case(Case::UpperSnake),
+                        datatype
+                    ),
+                };
+
+                result = result + &member;
+            } else {
+                panic!("Data type is unknown or undefined.");
+            }
+        }
+
+        result
+    }
+}
+
 pub(crate) struct StructFormatter;
 
 impl StructFormatter {
@@ -55,6 +82,11 @@ impl ObjectWriter {
         let object_type: Result<ObjectType, String> = prototype.class.parse();
 
         match object_type {
+            Ok(ObjectType::ENUM) => {
+                let mut members = EnumFormatter::format(prototype.members);
+                members = members + "}";
+                file.write(members.as_bytes())?;
+            }
             Ok(ObjectType::STRUCT) => {
                 let mut members = StructFormatter::format(prototype.members);
                 members = members + "}";
